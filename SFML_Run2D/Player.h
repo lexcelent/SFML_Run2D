f6594cbdb2f0f4458b04b1a55f6e7fbd34dafc1a
lexcelent <lexanorilsk@gmail.com>
2022-10-06 16:44:39 +0300

    Add animation, collision

--- SFML_Run2D/Player.h
+++ SFML_Run2D/Player.h
@@ -3,8 +3,29 @@
 
 #include <SFML/Graphics.hpp>
 
-// temporary solution
-const int Ground = 150;
+// Ground of the map
+const int Ground = 550;
+
+// Map size
+const int H = 12; // Height
+const int W = 12; // Width
+const int TileSize = 32;
+
+
+sf::String TileMap[H]{
+	"BBBBBBBBBBBB",
+	"B          B",
+	"B          B",
+	"B          B",
+	"B          B",
+	"B          B",
+	"B          B",
+	"B          B",
+	"B          B",
+	"B          B",
+	"B          B",
+	"BBBBBBBBBBBB",
+};
 
 class Player
 {
@@ -14,18 +35,27 @@ public:
 	bool onGround = false;
 	sf::Texture texture;
 	sf::Sprite sprite;
+	float currentFrame; // animation
 
 	Player() {
 		texture.loadFromFile("images/player.png");
 		sprite.setTexture(texture);
-		rect = sf::FloatRect(100, 100, 50, 50);
+		rect = sf::FloatRect(0, 0, 32, 32);
+		sprite.setTextureRect(sf::IntRect(0, 0, 32, 32));
+		
+		// start position
+		rect.left = 64;
+		rect.top = 64;
+		
 		dx = dy = 0;
+		currentFrame = 0;
 	}
-
+	
 	void update(float time)
 	{
 		// rect.left is X
 		rect.left += dx * time;
+		Collision(0);
 
 		// 0.0005 is acceleration
 		// acceleration * time = speed
@@ -34,16 +64,47 @@ public:
 		// rect.top is Y
 		rect.top += dy * time;
 		onGround = false;
+		Collision(1);
 
+		//
+		// Use this code if you don't have collision check
+		// and you are using "Ground" 
+		// 
+		// stay on the Ground
+		/*
 		if (rect.top > Ground) {
 			rect.top = Ground;
 			dy = 0;
 			onGround = true;
 		}
+		*/
+
+		// animation
+		currentFrame += 0.005 * time;
+		if (currentFrame > 2) currentFrame -= 2;
+
+		if (dx > 0) sprite.setTextureRect(sf::IntRect(0 + int(currentFrame) * 32, 32, 32, 32));
+		if (dx < 0) sprite.setTextureRect(sf::IntRect(0 + int(currentFrame) * 32 + 32, 32, -32, 32));
+		if (dx == 0) sprite.setTextureRect(sf::IntRect(0 + int(currentFrame) * 32, 0, 32, 32));
 
 		sprite.setPosition(rect.left, rect.top);
 
 		dx = 0;
+	}
+
+	// dir is direction
+	// 0 is X, 1 is Y
+	void Collision(bool dir) {
+		for (int i = rect.top / TileSize; i < (rect.top + rect.height) / TileSize; i++)
+			for (int j = rect.left / TileSize; j < (rect.left + rect.width) / TileSize; j++)
+				if (TileMap[i][j] == 'B')
+				{
+					if ((dx > 0) && (dir == 0)) { rect.left = j * TileSize - rect.width; }
+					if ((dx < 0) && (dir == 0)) { rect.left = j * TileSize + TileSize; }
+					if ((dy > 0) && (dir == 1)) { rect.top = i * TileSize - rect.height; dy = 0; onGround = true; }
+					if ((dy < 0) && (dir == 1)) { rect.top = i * TileSize + rect.height; dy = 0; }
+				}
+			
 	}
 };
 
